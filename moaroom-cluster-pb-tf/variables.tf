78ab747a3c097aeb430b01abb3abb09605850dbd
Nayeon Keum <nkeum@lablup.com>
2023-08-28 18:51:47 +0900

    [feat/#2] Add: moaroom-cluster(aws eks) tf files

---
+++ moaroom-cluster-pb-tf/variables.tf
@@ -0,0 +1,37 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-2"
+}
+variable "azs" {
+  description = "A list of availability zones names or ids in the region"
+  type = list(string)
+  default = ["ap-northeast-2a", "ap-northeast-2c"]
+}
+
+variable "cluster_name" {
+  description = "Cluster Name"
+  type = string
+  default = null
+}
+
+variable "cluster_version" {
+  description = "Cluster Version"
+  type = string
+  default = null
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR Range"
+  type = string
+  default = null
+}
+
+variable "vpc_name" {
+  description = "VPC Name"
+  type = string
+  default = null
+}
